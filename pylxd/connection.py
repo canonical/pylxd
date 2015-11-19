d0f8af67374b7ab97d295d25544a48c56529d03a
Chuck Short <chuck.short@canonical.com>
2015-11-19 10:40:14 -0500

    Detect SSL TLS version
    Ubuntu 14.04 doesnt have ssl.PROTOCOL_TLSv1_2, so check
    to see if we hvae ssl.PROTOCOL_TLSv1_2 available otherwise
    use OpenSSL.SSL.TLSv1_2_METHOD.
    
    Fixes issue #42
    
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- pylxd/connection.py
+++ pylxd/connection.py
@@ -22,6 +22,22 @@ from pylxd import exceptions
 from pylxd import utils
 from six.moves import http_client
 
+if hasattr(ssl, 'SSLContext'):
+    # For Python >= 2.7.9 and Python 3.x
+    USE_STDLIB_SSL = True
+else:
+    # For Python 2.6 and <= 2.7.8
+    USE_STDLIB_SSL = False
+
+if not USE_STDLIB_SSL:
+    import OpenSSL.SSL
+
+# Detect SSL tls version
+if hasattr(ssl, 'PROTOCOL_TLSv1_2'):
+    DEFAULT_TLS_VERSION = ssl.PROTOCOL_TLSv1_2
+else:
+    DEFAULT_TLS_VERSION = OpenSSL.SSL.TLSv1_2_METHOD
+
 
 class UnixHTTPConnection(http_client.HTTPConnection):
 
@@ -55,7 +71,7 @@ class HTTPSConnection(http_client.HTTPConnection):
         (cert_file, key_file) = self._get_ssl_certs()
         self.sock = ssl.wrap_socket(sock, certfile=cert_file,
                                     keyfile=key_file,
-                                    ssl_version=ssl.PROTOCOL_TLSv1_2)
+                                    ssl_version=DEFAULT_TLS_VERSION)
 
     @staticmethod
     def _get_ssl_certs():
