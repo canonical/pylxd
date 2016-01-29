f1216925454f5823a4ed61b6146039ea427d341a
Paul Hummer <paul.hummer@canonical.com>
2016-01-29 11:55:22 -0700

    Add more integration tests

--- integration/testing.py
+++ integration/testing.py
@@ -12,9 +12,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 import unittest
-import uuid
 
 from pylxd.api import LXD
+from integration.busybox import create_busybox_image
 
 
 class IntegrationTestCase(unittest.TestCase):
@@ -26,7 +26,7 @@ class IntegrationTestCase(unittest.TestCase):
 
     def create_container(self):
         """Create a container in lxd."""
-        name = 'a' + str(uuid.uuid4())
+        name = self.id().split('.')[-1].replace('_', '')
         machine = {
             'name': name,
             'architecture': 2,
@@ -38,12 +38,45 @@ class IntegrationTestCase(unittest.TestCase):
         }
         result = self.lxd['1.0']['containers'].post(json=machine)
         operation_uuid = result.json()['operation'].split('/')[-1]
-        self.lxd['1.0'].operations[operation_uuid].wait.get()
+        result = self.lxd['1.0'].operations[operation_uuid].wait.get()
+
+        self.addCleanup(self.delete_container, name)
         return name
 
-    def delete_container(self, name):
+    def delete_container(self, name, enforce=False):
         """Delete a container in lxd."""
-        self.lxd['1.0']['containers'][name].delete()
+        #response = self.lxd['1.0'].containers['name'].get()
+        #if response == 200:
+        # enforce is a hack. There's a race somewhere in the delete.
+        # To ensure we don't get an infinite loop, let's count.
+        count = 0
+        result = self.lxd['1.0']['containers'][name].delete()
+        while enforce and result.status_code == 404 and count < 10:
+            result = self.lxd['1.0']['containers'][name].delete()
+            count += 1
+        try:
+            operation_uuid = result.json()['operation'].split('/')[-1]
+            result = self.lxd['1.0'].operations[operation_uuid].wait.get()
+        except KeyError:
+            pass  # 404 cases are okay.
+
+    def create_image(self):
+        """Create an image in lxd."""
+        path, fingerprint = create_busybox_image()
+        with open(path, 'rb') as f:
+            headers = {
+                'X-LXD-Public': '1',
+                }
+            response = self.lxd['1.0'].images.post(data=f.read(), headers=headers)
+        operation_uuid = response.json()['operation'].split('/')[-1]
+        self.lxd['1.0'].operations[operation_uuid].wait.get()
+
+        self.addCleanup(self.delete_image, fingerprint)
+        return fingerprint
+
+    def delete_image(self, fingerprint):
+        """Delete an image in lxd."""
+        self.lxd['1.0'].images[fingerprint].delete()
 
     def assertCommon(self, response):
         """Assert common LXD responses.
