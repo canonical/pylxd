d16123d776d8fe5b526b3ba81aa3ff9e8ddb87ce
Paul Hummer <paul@eventuallyanyway.com>
2016-05-22 22:02:57 -0600

    Update api example for PyLXD 2.0 API

--- examples/api_test.py
+++ examples/api_test.py
@@ -16,24 +16,17 @@
 
 import uuid
 
-from pylxd import api
+from pylxd.client import Client
 
 # Let's pick a random name, avoiding clashes
-CONTAINER_NAME = str(uuid.uuid1())
+CONTAINER_NAME = str(uuid.uuid1()).split('-')[0]
 
-lxd = api.API()
-try:
-    lxd.container_defined(CONTAINER_NAME)
-except Exception as e:
-    print("Container doesnt exist: %s" % e)
+lxd = Client()
 
-config = {'name': CONTAINER_NAME,
-          'source': {'type': 'none'}}
-lxd.container_init(config)
-if lxd.container_defined(CONTAINER_NAME):
-    print("Container is running")
-else:
-    print("Whoops!")
-containers = lxd.container_list()
-for x in containers:
-    lxd.container_destroy(x)
+container_config = {
+    'name': CONTAINER_NAME,
+    'source': {'type': 'none'},
+}
+container = lxd.containers.create(container_config, wait=True)
+
+container.delete(wait=True)
