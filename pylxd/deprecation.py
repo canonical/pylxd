cff0599bbc891ec690f818cb85bffce3e78212df
Paul Hummer <paul@eventuallyanyway.com>
2016-05-29 21:30:50 -0600

    Fix pep8

--- pylxd/deprecation.py
+++ pylxd/deprecation.py
@@ -11,13 +11,15 @@ class deprecated():
     be removed.
     """
 
+    DEFAULT_MESSAGE = '{} is deprecated and will be removed soon.'
+
     def __init__(self, message=None):
         self.message = message
 
     def __call__(self, f):
         def wrapped(*args, **kwargs):
             if self.message is None:
-                self.message = '{} is deprecated and will be removed soon.'.format(
+                self.message = self.DEFAULT_MESSAGE.format(
                     f.__name__)
             warnings.warn(self.message, DeprecationWarning)
             return f(*args, **kwargs)
