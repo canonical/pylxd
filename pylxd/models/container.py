4d192f6f81dabff416b9f8994c727453582a8737
atx <atx@drecks-provider.de>
2017-02-04 22:48:18 +0100

    include created_at for snapshot

--- pylxd/models/container.py
+++ pylxd/models/container.py
@@ -356,6 +356,7 @@ class Snapshot(model.Model):
     """A container snapshot."""
 
     name = model.Attribute()
+    created_at = model.Attribute()
     stateful = model.Attribute()
 
     container = model.Parent()
