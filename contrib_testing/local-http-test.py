58d8aa68b75032be2a4f4983bd11281706118c43
Alex Kavanagh <alex@ajkavanagh.co.uk>
2018-03-19 17:36:18 +0000

    Add testing files that demonstrate the issue
    These files demonstrated the bug/280 issue and that the associated
    fix works.  However, their nature makes them tricky to incorporate into
    a gating integration test due to networking requirements in the
    containers.
    
    Signed-off-by: Alex Kavanagh <alex@ajkavanagh.co.uk>

---
+++ contrib_testing/local-http-test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import datetime
+import pylxd
+import requests
+import time
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def log(s):
+    now = datetime.datetime.utcnow()
+    print("{} - {}".format(now, s))
+
+
+def create_and_update(client):
+    log("Creating...")
+    base = client.containers.create({
+        'name': 'ubuntu-1604',
+        'source': {
+            'type': 'image',
+            'protocol': 'simplestreams',
+            'server': 'https://images.linuxcontainers.org',
+            'alias': 'ubuntu/xenial/amd64'
+        }
+    }, wait=True)
+    log("starting...")
+    base.start(wait=True)
+    while len(base.state().network['eth0']['addresses']) < 2:
+        time.sleep(1)
+    commands = [
+        ['apt-get', 'update'],
+        ['apt-get', 'install', 'openssh-server', 'sudo', 'man', '-y']
+    ]
+    for command in commands:
+        log("command: {}".format(command))
+        result = base.execute(command)
+        log("result: {}".format(result.exit_code))
+        log("stdout: {}".format(result.stdout))
+        log("stderr: {}".format(result.stderr))
+
+
+if __name__ == '__main__':
+    client = pylxd.Client("https://127.0.0.1:8443/", verify=False)
+    log("Authenticating...")
+    client.authenticate('password')
+
+    create_and_update(client)
