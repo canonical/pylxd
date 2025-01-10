7e827f943161095269a1ee6739f5bc6bb4a46073
hamistao <pedro.ribeiro@canonical.com>
2025-01-09 23:19:11 -0300

    integration/test_containers: Give containers some time before exec
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- integration/test_containers.py
+++ integration/test_containers.py
@@ -11,6 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import time
 import unittest
 
 from integration.testing import IntegrationTestCase
@@ -145,6 +146,9 @@ class TestContainer(IntegrationTestCase):
         """A command is executed on the container."""
         self.container.start(wait=True)
         self.addCleanup(self.container.stop, wait=True)
+        time.sleep(
+            1
+        )  # Wait a little to make sure the container is ready to exec since it has just been started.
 
         result = self.container.execute(["echo", "test"])
 
@@ -156,6 +160,9 @@ class TestContainer(IntegrationTestCase):
         """A command is executed on the container without buffering the output."""
         self.container.start(wait=True)
         self.addCleanup(self.container.stop, wait=True)
+        time.sleep(
+            1
+        )  # Wait a little to make sure the container is ready to exec since it has just been started.
         buffer = []
 
         result = self.container.execute(["echo", "test"], stdout_handler=buffer.append)
@@ -169,6 +176,9 @@ class TestContainer(IntegrationTestCase):
         """A command is executed on the container that isn't utf-8 decodable"""
         self.container.start(wait=True)
         self.addCleanup(self.container.stop, wait=True)
+        time.sleep(
+            1
+        )  # Wait a little to make sure the container is ready to exec since it has just been started.
 
         result = self.container.execute(["printf", "\\xff"], decode=None)
 
@@ -180,6 +190,9 @@ class TestContainer(IntegrationTestCase):
         """A command is executed and force output to ascii"""
         self.container.start(wait=True)
         self.addCleanup(self.container.stop, wait=True)
+        time.sleep(
+            1
+        )  # Wait a little to make sure the container is ready to exec since it has just been started.
 
         result = self.container.execute(
             ["printf", "qu\\xe9"], decode=True, encoding="latin1"
@@ -193,6 +206,9 @@ class TestContainer(IntegrationTestCase):
         """A command receives data from stdin and write to stdout handler"""
         self.container.start(wait=True)
         self.addCleanup(self.container.stop, wait=True)
+        time.sleep(
+            1
+        )  # Wait a little to make sure the container is ready to exec since it has just been started.
         test_msg = "Hello world!\n"
         stdout_msgs = []
 
