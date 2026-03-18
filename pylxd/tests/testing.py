8dc4a4be19417b040196d6213a0a7d1af3ed54fe
Zixing Liu <zixing.liu@canonical.com>
2026-03-18 15:56:40 +0800

    tests: delete mock-services and upgrade requests-mock
    Newer version of requests-mock provides an easier way to add match
    rules. There is no longer a need for mock-services.
    
    Signed-off-by: Zixing Liu <zixing.liu@canonical.com>

--- pylxd/tests/testing.py
+++ pylxd/tests/testing.py
@@ -1,7 +1,8 @@
 import json
+import re
 import unittest
 
-import mock_services
+import requests_mock
 
 from pylxd.client import Client
 from pylxd.tests import mock_lxd
@@ -11,20 +12,29 @@ class PyLXDTestCase(unittest.TestCase):
     """A test case for handling mocking of LXD services."""
 
     def setUp(self):
-        mock_services.update_http_rules(mock_lxd.RULES)
-        mock_services.start_http_mock()
+        self.requests_mock = requests_mock.Mocker()
+        self.add_rules(mock_lxd.RULES)
+        self.requests_mock.start()
 
         self.client = Client(endpoint="http://pylxd.test")
 
     def tearDown(self):
-        mock_services.stop_http_mock()
+        self.requests_mock.stop()
 
     def add_rule(self, rule):
         """Add a rule to the mock LXD service."""
-        mock_services.update_http_rules([rule])
+        self.requests_mock.register_uri(
+            rule["method"],
+            re.compile(rule["url"]),
+            text=rule.get("text"),
+            status_code=rule.get("status_code", 200),
+            json=rule.get("json"),
+            headers=rule.get("headers", {}),
+        )
 
     def add_rules(self, rules):
-        mock_services.update_http_rules(rules)
+        for rule in rules:
+            self.add_rule(rule)
 
 
 def add_api_extension_helper(obj, extensions):
