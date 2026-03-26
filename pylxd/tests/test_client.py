2cdca37e81eaa1e556437b5645cf53fdebb22e69
Simon Deziel <simon.deziel@canonical.com>
2026-03-26 17:28:00 -0400

    pylxd/tests/test_client: add test for `_ws_exclude_origin` helper
    Suggested-by: Tuğba Taluy <tugba.taluy@canonical.com>
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- pylxd/tests/test_client.py
+++ pylxd/tests/test_client.py
@@ -656,3 +656,23 @@ class TestGetSessionForUrl(TestCase):
         """If verify is given, it's set on the Session."""
         session = client.get_session_for_url("http://test.com", verify=True)
         self.assertIs(session.verify, True)
+
+
+class TestWsExcludeOrigin:
+    """Tests for pylxd.client._ws_exclude_origin."""
+
+    @pytest.mark.parametrize(
+        "input_kwargs,expected",
+        [
+            ({}, ["origin"]),
+            ({"exclude_headers": ["upgrade"]}, ["upgrade", "origin"]),
+            (
+                {"exclude_headers": ["Origin"]},
+                ["Origin"],
+            ),  # already present, case-insensitive
+            ({"exclude_headers": None}, ["origin"]),
+        ],
+    )
+    def test_ws_exclude_origin(self, input_kwargs, expected):
+        client._ws_exclude_origin(input_kwargs)
+        assert input_kwargs["exclude_headers"] == expected
